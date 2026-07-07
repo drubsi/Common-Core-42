983823b6160b8a3e9357d2bde38d35537fdaa74a
David <davirubio@gmail.com>
2026-07-07 20:06:09 +0200

    feat: add main entry point.

---
+++ Milestone05/CPP/CPP06/ex00/main.cpp
@@ -0,0 +1,14 @@
+#include "ScalarConverter.hpp"
+#include <iostream>
+#include <string>
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "To execute the program there must be a literal type parameter. ./convert <parameter>." << std::endl;
+		return(1);
+	}
+	ScalarConverter::convert(argv[1]);
+	return (0);
+}
